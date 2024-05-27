c6cb5bb341fc3feaaa6e1a7b040736d024738a9f
gbcostasant@gmail.com <gbcostasant@gmail.com>
2024-05-27 19:39:22 -0300

    Load game and startedcoding the instructions

--- main.go
+++ main.go
@@ -3,8 +3,8 @@ package main
 import (
 	"log"
 
-    "github.com/hajimehoshi/ebiten/v2"
-    "github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
 type Game struct{}
@@ -22,10 +22,12 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 }
 
 func main() {
-	ebiten.SetWindowSize(640, 480)
-	ebiten.SetWindowTitle("Hello, World!")
-    initChip8()
-	if err := ebiten.RunGame(&Game{}); err != nil {
-		log.Fatal(err)
-	}
+  ebiten.SetWindowSize(960, 720)
+  ebiten.SetWindowTitle("Hello, World!")
+
+  initChip8()
+  loadROM()
+  if err := ebiten.RunGame(&Game{}); err != nil {
+    log.Fatal(err)
+  }
 }
